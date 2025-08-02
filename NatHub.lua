00489571f08f91ccd13d0fef77fb2e44ed67388d
scripts <nielanthonyr@gmail.com>
2025-08-02 16:26:47 +0800

    NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/5kBUTtqLNk0dbGcx/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/paSMTQR6jy9j0UXG/raw", true))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
