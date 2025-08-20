fbc7a8d96c7bc97e6a14713eadae8403ab0915ea
scripts <nielanthonyr@gmail.com>
2025-08-20 14:01:55 +0800

    NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/34qV9RKX/raw"))()
+ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
