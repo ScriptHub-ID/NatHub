f23943aa3cc89ea410066f48dc79c6e8e1617ca8
scripts <nielanthonyr@gmail.com>
2025-09-06 13:51:28 +0800

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/uCl1cwsD/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/ksXe3kJm/raw"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
